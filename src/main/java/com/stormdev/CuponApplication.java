160d0f57210ed13a049d0801e7b2cdcb3b418fda
Vinicius Ribeiro <viniribeiroa@gmail.com>
2026-03-08 12:21:38 -0300

    fist commit

---
+++ src/main/java/com/stormdev/CuponApplication.java
@@ -0,0 +1,13 @@
+package com.stormdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuponApplication.class, args);
+	}
+
+}
