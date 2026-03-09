1fbb227035a03139e1451bcd4b235a1c35c02438
Vinicius Ribeiro <viniribeiroa@gmail.com>
2026-03-08 21:42:28 -0300

    CouponStatus

---
+++ src/main/java/com/stormdev/domain/model/CouponStatus.java
@@ -0,0 +1,13 @@
+/**
+ * Autor: VINICIUS
+ * Data: 8 de mar. de 2026
+ * Descrição: TODO
+ */
+package com.stormdev.domain.model;
+
+public enum CouponStatus {
+    ACTIVE,
+    EXPIRED,
+    DELETED,
+    REDEEMED
+}
